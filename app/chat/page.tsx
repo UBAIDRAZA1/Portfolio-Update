3cef4fa05e00e97536a9c75a6f05ed42ea86bfad
Ubaid Raza <141560709+UBAIDRAZA1@users.noreply.github.com>
2026-07-17 15:25:40 +0500

    Create ChatPage that redirects to external URL
    chat bot Agent add

---
+++ app/chat/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function ChatPage() {
+  redirect("https://www.sketricgen.ai/fullscreen-widget/skbrand_af29dbb8-4a23-46ec-ae80-d0df492f869c");
+}
